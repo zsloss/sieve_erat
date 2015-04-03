d5c1cad681d82f7da2a5ab213d89f76d335103f4
Zachary Sloss <zachary.sloss@gmail.com>
2015-04-03 08:26:30 +0000

    Add (much!) improved vector function

--- main.cpp
+++ main.cpp
@@ -23,6 +23,30 @@ std::vector<int> vec_sieve(const int max) {
 	return nums;
 }
 
+std::vector<int> improved_vec_sieve(const int max) {
+
+	// Fill new collection with sequence up to MAX
+	std::vector<int> nums(max + 1);
+	std::iota(nums.begin(), nums.end(), 0);
+	
+	auto it = nums.begin() + 2;
+
+	while (it != nums.end()) {
+		int prime = *it;
+		for (auto i = prime * 2; i < max + 1; i += prime) {
+			nums[i] = 0;
+		}
+
+		do {
+			++it;
+		} while (it != nums.end() && *it == 0);
+	}
+		// Remove all numbers divisible by current prime.
+	nums.erase(std::remove_if(nums.begin(), nums.end(), [](const int i){ return i < 2; }), nums.end());
+
+	return nums;
+}
+
 std::list<int> list_sieve(const int max) {
 
 	// Fill new collection with sequence up to MAX
@@ -36,7 +60,7 @@ std::list<int> list_sieve(const int max) {
 
 		// Remove all numbers divisible by current prime.
 		nums.remove_if([curr_prime](const int i){ return i != curr_prime && i % curr_prime == 0; });
-		it++;
+		++it;
 	}
 	return nums;
 }
@@ -57,7 +81,7 @@ inline double time_in_secs (clock_t t) {
 int main() {
 
 	const int MAX = 100000;
-	clock_t t_list, t_vec;
+	clock_t t_list, t_vec, t_ivec;
 
 	// LIST
 	t_list = clock();
@@ -75,7 +99,16 @@ int main() {
 	std::cout << "vector\n\n";
 	print_primes(vec_primes);
 
+	// IMPROVED VECTOR
+	t_ivec = clock();
+	auto ivec_primes = improved_vec_sieve(1000000);
+	t_ivec = clock() - t_ivec;
+
+	std::cout << "improved vector\n\n";
+	print_primes(ivec_primes);
+
 	std::cout << "Using a list took " << time_in_secs(t_list) << " seconds.\n\n";
 	std::cout << "Using a vector took " << time_in_secs(t_vec) << " seconds.\n\n";
+	std::cout << "Using an improved vector took " << time_in_secs(t_ivec) << " seconds.\n\n";
 }
 
